ea1ed3238a4906b9880700cd229c0875056b4940
James Coughlin <Jcoughlin91@gmail.com>
2022-07-29 13:13:10 -0500

    updated hw7

--- components/views/Bio.js
+++ components/views/Bio.js
@@ -1,3 +1,8 @@
 import html from "html-literal";
 
-export default () => html``;
+export default () => html`
+  <h3>
+    The weather in ${st.weather.city} is ${st.weather.description}. Temperature
+    is ${st.weather.temp}F, and it feels like ${st.weather.feelsLike}F.
+  </h3>
+`;
