4fe4eac92640c1f2f2fd9d4285f92c98fcca0e9d
James Coughlin <Jcoughlin91@gmail.com>
2022-07-29 13:27:52 -0500

    updated hw7

--- index.js
+++ index.js
@@ -23,38 +23,69 @@ function afterRender() {
     document.querySelector("nav > ul").classList.toggle("hidden--mobile");
   });
 }
-
 router.hooks({
   before: (done, params) => {
     const view =
       params && params.data && params.data.view
         ? capitalize(params.data.view)
-        : "Bio";
-        switch (view) {
-          case "Home": {
-            axios
-              .get(
-                `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis`
-              )
-              .then(response => {
-                const kelvinToFahrenheit = kelvinTemp =>
-                  Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+        : “Bio”;
+    // Add a switch case statement to handle multiple routes
+    switch (view) {
+      case “Bio”: {
+        axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis`
+          )
+          .then((response) => {
+            const kelvinToFahrenheit = (kelvinTemp) =>
+              Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+            store.Bio.weather = {};
+            store.Bio.weather.city = response.data.name;
+            store.Bio.weather.temp = kelvinToFahrenheit(
+              response.data.main.temp
+            );
+            store.Bio.weather.feelsLike = kelvinToFahrenheit(
+              response.data.main.feels_like
+            );
+            store.Bio.weather.description = response.data.weather[0].main;
+            done();
+          })
+          .catch((err) => console.log(err));
+        break;
+      }
+      done();
 
-                store.Bio.weather = {};
-                store.Bio.weather.city = response.data.name;
-                store.Bio.weather.temp = kelvinToFahrenheit(
-                  response.data.main.temp
-                );
-                store.Bio.weather.feelsLike = kelvinToFahrenheit(
-                  response.data.main.feels_like
-                );
-                store.Bio.weather.description = response.data.weather[0].main;
-                done();
-              })
-              .catch(err => console.log(err));
-            break;
-          }
-done();
+// router.hooks({
+//   before: (done, params) => {
+//     const view =
+//       params && params.data && params.data.view
+//         ? capitalize(params.data.view)
+//         : "Bio";
+//         switch (view) {
+//           case "Home": {
+//             axios
+//               .get(
+//                 `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis`
+//               )
+//               .then(response => {
+//                 const kelvinToFahrenheit = kelvinTemp =>
+//                   Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+
+//                 store.Bio.weather = {};
+//                 store.Bio.weather.city = response.data.name;
+//                 store.Bio.weather.temp = kelvinToFahrenheit(
+//                   response.data.main.temp
+//                 );
+//                 store.Bio.weather.feelsLike = kelvinToFahrenheit(
+//                   response.data.main.feels_like
+//                 );
+//                 store.Bio.weather.description = response.data.weather[0].main;
+//                 done();
+//               })
+//               .catch(err => console.log(err));
+//             break;
+//           }
+// done();
 
 router
   .on({
